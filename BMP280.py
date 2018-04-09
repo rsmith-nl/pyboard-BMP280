9957042ff742ff919406805a06a2a6a0ce5c5aac
Roland Smith <rsmith@xs4all.nl>
2018-04-09 20:59:45 +0200

    Aanpassing in de code, resultaat.

--- BMP280.py
+++ BMP280.py
@@ -4,7 +4,7 @@
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2018-04-08 22:38:40 +0200
-# Last modified: 2018-04-09 16:11:47 +0200
+# Last modified: 2018-04-09 20:49:53 +0200
 
 from utime import sleep_ms
 from ustruct import unpack
@@ -16,6 +16,7 @@ REG_ID = const(0xD0)  # id register
 ID_VAL = const(0x58)  # id value
 REG_STATUS = const(0xF3)  # status register
 REG_CONFIG = const(0xF5)  # configuration register
+REG_CONTROL = const(0xF4)  # configuration register
 REG_PRESS = const(0xF7)  # Start of data registers.
 # All registers F7 - F9 should be read in one go.
 REG_TEMP = const(0xFA)
@@ -26,17 +27,15 @@ REG_COMP = const(0x88)  # Start of compensation parameters.
 
 class BMP280_I2C:
 
-    def __init__(self, bus, address=0x77):
+    def __init__(self, bus, address=0x76):
         """Create an BMP280_I2C object.
 
         Arguments:
             bus: which connections we use.
             address: I2C address. Defaults to 0x76
         """
-        self._i2c = I2c(bus, I2C.MASTER)
+        self._i2c = I2C(bus, I2C.MASTER)
         self._address = address
-        if self.ready:
-            self._i2c.mem_write(0xF3, self._address, REG_CONFIG)
         self._temp = None
         self._press = None
         # Read the compensation coefficients.
@@ -51,7 +50,7 @@ class BMP280_I2C:
         """Whether BMP280 is found and available."""
         # if not self._i2c.is_ready(self._address):
         #    return False
-        if self._i2c.mem_read(1, self._address, REG_ID) != ID_VAL:
+        if self._i2c.mem_read(1, self._address, REG_ID)[0] != ID_VAL:
             return False  # Not a BMP280!
         return True
 
@@ -63,31 +62,53 @@ class BMP280_I2C:
     def pressure(self):
         return self._press
 
-    def update(self):
+    def read(self):
         """Read the sensor data from the chip."""
-        if self._i2c.mem_read(1, self._address, REG_STATUS) & 0x08 == 1:
+        # Do one measurement in high resolution, forced mode.
+        self._i2c.mem_write(0xFE, self._address, REG_CONTROL)
+        while self._i2c.mem_read(1, self._address, REG_STATUS)[0] & 0x08:
             sleep_ms(2)  # Busy measuring.
         # Temperature
         tempdata = self._i2c.mem_read(3, self._address, REG_TEMP)
         UT = float((tempdata[0] << 16 | tempdata[1] << 8 | tempdata[2]) >> 4)
+        print("DEBUG: UT = ", UT)
         var1 = (UT / 16384.0 - self._tempcal[0] / 1024.0) * self._tempcal[1]
+        print("DEBUG: var1 = ", var1)
         var2 = ((UT / 131072.0 - self._tempcal[0] / 8192.0) * (
             UT / 131072.0 - self._tempcal[0] / 8192.0)) * self._tempcal[2]
+        print("DEBUG: var2 = ", var2)
         t_fine = int(var1+var2)
+        print("DEBUG: t_fine = ", t_fine)
         self._temp = t_fine/5120.0
+        print("DEBUG: self._temp = ", self._temp)
         # Pressure
         pressdata = self._i2c.mem_read(3, self._address, REG_PRESS)
         UP = float((pressdata[0] << 16 | pressdata[1] << 8 | tempdata[2]) >> 4)
+        print("DEBUG: UP = ", UP)
         var1 = t_fine/2.0 - 64000.0
+        print("DEBUG: var1 = ", var1)
         var2 = var1 * var1 * self._presscal[5] / 32768.0
+        print("DEBUG: var2 = ", var2)
         var2 = var2 + var1 * self._presscal[4] * 2.0
+        print("DEBUG: var2 = ", var2)
         var2 = var2/4.0 + self._presscal[3] * 65536.0
-        var1 = self._presscal[2] * var1 * var1 / 534288.0 + self._presscal[1] * var1 / 534288.0
+        print("DEBUG: var2 = ", var2)
+        var1 = (self._presscal[2] * var1 * var1 / 534288.0 +
+                self._presscal[1] * var1) / 534288.0
+        print("DEBUG: var1 = ", var1)
         var1 = (1.0 + var1/32768.0) * self._presscal[0]
+        print("DEBUG: var1 = ", var1)
         if var1 == 0.0:
             return 0
         p = 1048576.0 - UP
-        p = (p - var2 / 4096.0) * 6250 / var1
+        print("DEBUG: p = ", p)
+        p = ((p - var2 / 4096.0) * 6250) / var1
+        print("DEBUG: p = ", p)
         var1 = self._presscal[8] * p * p / 2147483648.0
+        print("DEBUG: var1 = ", var1)
         var2 = p * self._presscal[7] / 32768.0
-        self._press = p + (var1 + var2 + self._presscal[6]) / 16.0
+        print("DEBUG: var2 = ", var2)
+        p = p + (var1 + var2 + self._presscal[6]) / 16.0
+        self._press = p
+        print("DEBUG: self._press = ", self._press)
+        return (self._temp, self._press)
