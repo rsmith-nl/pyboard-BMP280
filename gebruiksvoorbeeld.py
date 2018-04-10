dc0c8ed13586baa11c9dbd31ef838da99e2ea88e
Roland Smith <rsmith@xs4all.nl>
2018-04-10 15:10:03 +0200

    Remove superfluous example.

--- gebruiksvoorbeeld.py
+++
@@ -1,9 +0,0 @@
-import BMP280
-s = BMP280.BMP280_I2C(2)
-s.read()
-
-data = s._i2c.mem_read(24, s._address, BMP280.REG_COMP)
-
-from ustruct import unpack
-comp = unpack('<HhhHhhhhhhhh', data)
-s._i2c.mem_read(2, s._address, BMP280.REG_COMP)
