66c1db32535d45fd29c199c19ff319986dfc8329
Roland Smith <rsmith@xs4all.nl>
2018-04-11 12:12:18 +0200

    Explicitly import from pyb to silence checker.

--- main-continuous.py
+++ main-continuous.py
@@ -3,7 +3,7 @@
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2018-04-10 14:39:42 +0200
-# Last modified: 2018-04-10 17:34:47 +0200
+# Last modified: 2018-04-11 12:10:40 +0200
 #
 # To the extent possible under law, R.F. Smith has waived all copyright and
 # related or neighboring rights to main.py. This work is published
@@ -12,22 +12,19 @@
 Create a BMP280 sensor object. Then query it every minute and print the results
 over the serial connection.
 
-copy this program to the pyboard as “main.py”.
+Copy this program to the pyboard as “main.py”.
 """
-
+from pyb import delay
 from BMP280 import BMP280_I2C
-
-DELAY = const(60000)  # One minute in ms.
 
 s = BMP280_I2C(2)  # Assuming the chip is wired to the 2nd I²C bus.
 
-print('Please wait...')
-pyb.delay(DELAY)  # Wait a minute after startup.
+print('Please wait one minute for the sensor to settle...')
+delay(60000)
 print('Ready')
 
 while True:
-    # Read and print the values.
+    # Read and print the values, then wait.
     rv = s.read()
     print('{:.1f} °C, {:.0f} mbar'.format(s.temperature, s.mbar))
-    # Wait 60 seconds
-    pyb.delay(DELAY)
+    delay(60000)
--- main-query.py
+++ main-query.py
@@ -3,24 +3,23 @@
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2018-04-10 14:39:42 +0200
-# Last modified: 2018-04-10 17:41:24 +0200
+# Last modified: 2018-04-11 12:07:31 +0200
 #
 # To the extent possible under law, R.F. Smith has waived all copyright and
 # related or neighboring rights to main.py. This work is published
 # from the Netherlands. See http://creativecommons.org/publicdomain/zero/1.0/
 """
-Create a BMP280 sensor object.
-When a “?” is input, query the sensor and report the result.
+Create a BMP280 sensor object. When a line containing “?” is input over the
+serial connection, query the sensor and report the result. (That goed out over
+the serial connection.)
 
 Copy this program to the pyboard as “main.py”.
 """
-
+from pyb import delay
 from BMP280 import BMP280_I2C
 
-s = BMP280_I2C(2)  # Assuming the chip is wired to the 2nd I²C bus.
-
-DELAY = const(60000)  # One minute in ms.
-pyb.delay(DELAY)  # Wait a minute after startup.
+s = BMP280_I2C(2)  # Assuming the sensor is wired to the 2nd I²C bus.
+delay(60000)  # Wait a minute after startup; give the sensor time to settle.
 
 while True:
     if '?' in input():
